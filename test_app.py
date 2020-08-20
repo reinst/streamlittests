1ae1416d651430771553b55083e789f3661920cc
Reinst <reinst@users.noreply.github.com>
2020-08-20 08:44:59 -0500

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+import requests
+
+st.title("Tuts are fun")
+
+st.success("Successful")
+
+
+def main():
+    menu = ["Simp", "Limp", "Dimp"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    if choice == "Simp":
+        st.subheader("Calc Simp")
+        r = requests.get('http://worldtimeapi.org/api/ip')
+        data = r.json()
+        st.write(data)
+
+    elif choice == "Limp":
+        st.subheader("Calc Limp")
+
+    else:
+        st.subheader("Calc Dimp")
+
+if __name__ == '__main__':
+    main()
+
+                               
