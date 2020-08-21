6047ac3b11c5029b820820d48916c3a695c1abad
Your Name <you@example.com>
2020-08-21 12:12:57 -0700

    update test08

--- test_app.py
+++ test_app.py
@@ -20,11 +20,11 @@ def main():
 
     elif choice == "Limp":
         st.subheader("Calc Limp")
-            if choice == "Simp":
-            st.subheader("Calc Simp")
-            r = requests.get('http://worldtimeapi.org/api/ip')
-            data = r.json()
-            st.write(data)
+        if choice == "Simp":
+        st.subheader("Calc Simp")
+        r = requests.get('http://worldtimeapi.org/api/ip')
+        data = r.json()
+        st.write(data)
 
 
     else:
