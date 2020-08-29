3231b6190f4a1a105dc38a89cf4574ce73e15b4e
Your Name <you@example.com>
2020-08-28 19:36:50 -0700

    update 0004

--- test_app.py
+++ test_app.py
@@ -2,40 +2,37 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import requests
 import paramiko
+import json
+
+
+if st.button("start"):
+    st.write("starting")
+else:
+    st.write("stopping")
+
+# pick server
+options1 = st.selectbox('pick server type',('cm', 'lda', 'gda'))
+options2 = st.selectbox('pick server location',('ctc','elr'))
 
 st.title("Application Data Check")
-dataCenter = [ "CTC", "ELR" ]
-defaultDataCenter = 'ctc'
-choice1 = st.sidebar.selectbox("Select Data Center",dataCenter, key='1111')
-if choice1 == "ELR":
-    defaultDataCenter = 'elr'
-
-serverType = [ "GDA","CM", "LDA" ]
-defaultServerType = 'gda'
-choice2 = st.sidebar.selectbox("Select Server Type",serverType, key='2222')
-if choice2 == "CM":
-    defaultServerType = 'cm'
-elif choice2 == "LDA":
-    defaultServerType = 'lda'
-
 
 def sshConnection(nixCommand):
-    formatHostname = f"soa-{defaultDataCenter}-{defaultServerType}.uhc.com" 
+    formatHostname = f"Connecting to [ soa-{options1}-{options2}.uhc.com ]" 
     st.write(formatHostname)
 
     try:
         client = paramiko.SSHClient()
-        client.load_system_host_keys() # this loads any local ssh keys
+        client.load_system_host_keys() # this loads any local ssh keys #BEST to add specific host keys
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname='192.168.1.88', username='t', password='/home/t/.ssh/id_rsa')
+        client.connect(hostname='192.168.11.88', username='t', password='/home/t/.ssh/id_rsa', timeout=10)
         _, ss_stdout, ss_stderr = client.exec_command(nixCommand)
         r_out = ss_stdout.readlines()
         st.write(r_out)
         client.close()
         st.success("Command Executed")
     except:
-        st.write("Connection Error")
-        st.warning("Error")
+        st.write("Connection Error...")
+        st.warning("SSH Timeout, check connection or DNS")
 
 def main():
     systemCheck = ["","Auth", "Syslog" ]
