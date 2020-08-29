59b636adf37ed281f052693e9911043efd731640
Your Name <you@example.com>
2020-08-28 19:59:41 -0700

    update 0005

--- test_app.py
+++ test_app.py
@@ -2,21 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import requests
 import paramiko
-import json
 
-
-if st.button("start"):
-    st.write("starting")
-else:
-    st.write("stopping")
-
-# pick server
-options1 = st.selectbox('pick server type',('cm', 'lda', 'gda'))
-options2 = st.selectbox('pick server location',('ctc','elr'))
 
 st.title("Application Data Check")
 
-def sshConnection(nixCommand):
+def sshConnection(nixCommand): 
     formatHostname = f"Connecting to [ soa-{options1}-{options2}.uhc.com ]" 
     st.write(formatHostname)
 
@@ -24,7 +14,7 @@ def sshConnection(nixCommand):
         client = paramiko.SSHClient()
         client.load_system_host_keys() # this loads any local ssh keys #BEST to add specific host keys
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname='192.168.11.88', username='t', password='/home/t/.ssh/id_rsa', timeout=10)
+        client.connect(hostname='192.168.31.88', username='t', password='/home/t/.ssh/id_rsa', timeout=10)
         _, ss_stdout, ss_stderr = client.exec_command(nixCommand)
         r_out = ss_stdout.readlines()
         st.write(r_out)
@@ -35,10 +25,16 @@ def sshConnection(nixCommand):
         st.warning("SSH Timeout, check connection or DNS")
 
 def main():
+    global options1
+    global options2
     systemCheck = ["","Auth", "Syslog" ]
     choice3 = st.sidebar.selectbox("Log Checks",systemCheck, key='3333')
     systemAPI = ["","Alarms", "Updates" ]
     choice4 = st.sidebar.selectbox("API Checks",systemAPI, key='4444')
+
+    # pick server
+    options1 = st.selectbox('pick server type',('cm', 'lda', 'gda'))
+    options2 = st.selectbox('pick server location',('ctc','elr'))
 
     if choice3 == "Syslog":
         st.subheader("Syslog check")
