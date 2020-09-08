784fadb64812af6dc081f47a2f943908b8b24f0d
Your Name <you@example.com>
2020-09-08 14:30:53 -0700

    update 00101

--- test_app.py
+++ test_app.py
@@ -2,22 +2,28 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import requests
 import paramiko
+import configparser
+import os
 
 
-st.title("Troubleshooting Command Center")
+def getConfigData(section,element):
+    config = configparser.ConfigParser()
+    config.read('cmdapp_configs.ini') 
+    return config[section][element] 
 
 
 def sshConnection(nixCommand): 
-    formatHostname = f"Connected to: soa-{options1}-{options2}.uhc.com " 
+    formatHostname = f"Connected to: soa-{options1}-{options2}.uhc.com" 
+    connHost = '192.168.1.88'
     if customSearch:
-        st.write(f'Custom shell search [ {sentence} ]')
+        st.write(f'Custom shell search [ {customSearch} ]')
     else:
         st.write(f'Shell search: [ {nixCommand} ]')
     try:
         client = paramiko.SSHClient()
-        client.load_system_host_keys() # this loads any local ssh keys #BEST to add specific host keys
+        client.load_system_host_keys() #Edit key location in cmdapp_configs.ini 
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname='192.168.1.88', username='t', password='/home/t/.ssh/id_rsa', timeout=10)
+        client.connect(hostname=connHost, username='t', timeout=10)
         _, ss_stdout, ss_stderr = client.exec_command(nixCommand)
         r_out, r_err = ss_stdout.readlines(), ss_stderr.read()
         if len(r_err) < 5:
@@ -25,17 +31,19 @@ def sshConnection(nixCommand):
             st.write(r_out)
         else:
             st.error('Stderr returned')
+            st.write(r_err)
         client.close()
     except:
         st.write("Code Exception Error...")
         st.warning("Bad code, SSH Timeout, check connection, or DNS issue")
 
 
-
 def main():
     global options1
     global options2
     global customSearch
+
+    st.title("Command Center Troubleshooting Tool")
 
     systemCheck = ["","Auth", "Syslog" ]
     choice3 = st.sidebar.selectbox("Log Checks",systemCheck, key='3333')
@@ -60,7 +68,7 @@ def main():
             sshConnection(customSearch)
         else:
            # sshConnection(linuxCommand())
-            sshConnection('tail -n 10 /var/log/syslog')
+            sshConnection(getConfigData('linuxCommands','ha_logs'))
 
     if choice3 == "Auth":
         st.subheader("Check Type: Authentication")
